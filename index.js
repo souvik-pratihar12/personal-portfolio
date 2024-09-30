46fa6a4f476ebc8160757ac650ca1bd1738e180a
pratihar_29 <souvikpratihar1229@gmail.com>
2024-09-30 11:48:35 +0530

    personal portfolio
    Initial commit of personal portfolio website. Added the following:
    - Home page with introduction and overview.
    - About section detailing experience, education, and skills.
    - Projects section showcasing recent work with links to demos and GitHub repositories.
    - Contact form and social media links for easy communication.
    - Responsive design implemented using HTML, CSS (Flexbox/Grid), and JavaScript for interactivity.

---
+++ index.js
@@ -0,0 +1,255 @@
+
+<!---header--->
+
+<table border="0" width="100%" cellspacing="0" bgcolor="#673de6">
+<tr><td>
+<table border="0" width="85%" align="center" cellpadding="15">
+<tr>
+<td> <font face="Open Sans" size="5" color="black"> <b> Souvik's portfolio </b></font></td>
+
+<td width="20%"></td>
+<td> <a href="#"> <font color="#fff">Home </font> </a></td>
+<td> <a href="#"><font color="#fff">Services </font></a></td>
+<td> <a href="#"><font color="#fff">Skills </font></a></td>
+<td> <a href="#"><font color="#fff">Team </font> </a></td>
+<td> <a href="#"><font color="#fff">Contact Us </font> </a></td>
+</tr>
+</table>
+</td></tr>
+</table>
+
+<!---End header--->
+
+<!---Home---->
+<table>
+<tr><td>
+<table width="85%" border="0" align="center" cellpadding="15px">
+<tr>
+<td> 
+	<h1> <font face="Open Sans" size="5"> Hi, </font></h1>
+	<h1> <font face="Open Sans" size="5"> I'm Souvik Pratihar,My passion lies in coding and I find joy in creating seamless and user-friendly web experiences. The challenges of web development fuel my eagerness to learn and grow in this ever-evolving field. As an aspiring professional, 
+    I value staying updated with the latest trends and technologies </font></h1>
+	<font size="3"></font>
+
+<hr>
+<a href="assets/cv.pdf"><button> Download CV</button> </a>
+<a href="https://wa.link/emko8l"><button> Hire Me</button> </a>
+
+</td>
+
+<td>
+	<img src="assets/sp.jpeg" height="600px">
+</td>
+
+</tr>
+</table>
+</td></tr>
+</table>
+<!---End Home---->
+
+<!--- Services--->
+
+<table width="100%" bgcolor="#fafbff">
+<tr><td>
+<br><br><br><br>
+<table width="85%" border="0" align="center" cellpadding="15px">
+<tr align="center">
+<td colspan="3"> 
+	<font size="6" face="Open sans"> Services </font> <br>
+	<hr align="center" width="10%">
+	<font size="2" face="Open sans"> These are my Services </font> <br>
+  </td>
+</tr>
+<tr align="center">
+<td>
+	<img src="assets/appdev.png.png" width="15%"> <br><br>
+	<font size="5"> App Development </font> <br>
+	<font size="3"> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form </font>
+</td>
+<td>
+	<img src="assets/webdev.png.png" width="15%"> <br><br>
+	<font size="5"> Web Development </font> <br>
+	<font size="3"> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form </font>
+</td>
+
+</tr>
+</table>
+<br><br><br><br>
+</td></tr>
+<table>
+
+<!--- End Services--->
+<!--- Skills--->
+
+<table width="100%" bgcolor="#25282f">
+
+<tr>
+<td> <br><br><br><br>
+	<table border="0" width="85%" align="center" cellpadding="15px">
+	<tr align="center"> <td colspan="2"> <font face="Open Sans" size="6" color="#fff"> Skills </font>   
+	<hr align="center" width="7%">	 </td></tr>
+	<tr>
+	<td> <label> <font size="5" color="#fff"> HTML5 </font></label> <br><progress value="95" max="100">95% </progress> </td>
+	<td> <label> <font size="5" color="#fff"> CSS3 </font></label> <br><progress value="85" max="100">85% </progress> </td>
+	</tr>
+
+	<tr>
+	<td> <label> <font size="5" color="#fff"> JS </font></label> <br><progress value="65" max="100">65% </progress></td>
+	<td> <label> <font size="5" color="#fff"> php </font></label> <br><progress value="50" max="100">50% </progress></td>
+	</tr>
+	</table>
+<br><br><br><br>
+
+</td>
+</tr>
+</table>
+
+<!--- End Skills--->
+
+<!--- project--->
+<section id="work" class="work">
+    <div class="container">
+        <h1 class="section-heading"><span>My</span> Experiences</h1>
+        <p>I am a keen learner, gathering experience all the way</p>
+        <div class="card-wrapper">
+            <div class="card">
+                <div class="overlay">
+<section id="blog" class="blog">
+    <div class="container">
+        <h1 class="section-heading"><span>My</span>Projects</h1>
+    
+        <div class="card-wrapper">
+            <div class="card">
+                <div class="img-wrapper">
+                    <img src="assets/calcu.webp" alt="">
+                   
+                </div>
+                <div class="card-content">
+                    <a href="#">
+                        <h1>Calculator Project</h1>
+                    </a>
+                    <span>January, 2024</span>
+                    <p>Tech Stack: Python.</p>
+                        <button
+                        class="btn btn-color-2 project-btn"
+                        onclick="location.href='https:https://github.com/souvik-pratihar12'"
+                        >
+                    <a href="#">View Project</a>
+                </div>
+            </div>
+            
+            <div class="card">
+                <div class="img-wrapper">
+                    <img src="assets/pg.jpg" alt="">
+                </div>
+                <div class="card-content">
+                    <a href="#">
+                        <h1>Password Generator</h1>
+                    </a>
+                    <span>January, 2024</span>
+                    <p>Tech Stack:Python</p>
+                        <button
+                        class="btn btn-color-2 project-btn"
+                        onclick="location.href='https://github.com/souvik-pratihar12'"
+                        >
+                    <a href="#">View Project</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="img-wrapper">
+                    <img src="assets/to do.png" alt="">
+                </div>
+                <div class="card-content">
+                    <a href="#">
+                        <h1>To do list</h1>
+                    </a>
+                    <span>January, 2024</span>
+                    <p>Tech Stack:Python.</p>
+                        <button
+                        class="btn btn-color-2 project-btn" 
+                        onclick="location.href='https://github.com/souvik-pratihar12'"                             
+                        >
+                    <a href="#">View Project</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--- end project--->
+
+<!--- Contact--->
+<form>
+<table width="100%" bgcolor="#673de6">
+<tr><td>
+<br><br><br><br>
+<table border="0" width="30%" align="center" cellpadding="15">
+<tr align="center">
+<td colspan="2"> <font color="" size="6"> Contact Us </font> <br> <hr align="center" width="10%"> </td>
+</tr>
+<tr>
+<td> <font color="#fff">Name : </font> </td>
+<td> <input type="text"> </td>
+</tr>
+
+<tr>
+<td> <font color="#fff">Mobile : </font> </td>
+<td> <input type="number"> </td>
+</tr>
+
+<tr>
+<td> <font color="#fff"> Message : </font> </td>
+<td> <textarea cols="20" rows="4"> </textarea> </td>
+</tr>
+
+<tr align="center">
+<td colspan="2"> <input type="submit">  </td>
+</tr>
+</table><br><br><br><br>
+
+</td></tr>
+</table>
+</form>
+
+<!--- End Contact--->
+
+<!--- Footer--->
+
+<table width="100%">
+<tr><td>
+
+<table border="0" width="85%" align="center" cellpadding="15">
+<tr align="center">
+<td>
+<img src="assets/old-phone.png" width="25%"> <br>
+<font size="6" >Phone No.</font> <br>
+<font size="5">9064128125.</font>
+</td>
+
+<td>
+<img src="assets/email.png" width="25%"> <br>
+<font size="6" >Email us</font> <br>
+<font size="5" >souvikpratihar1229@gmail.com</font>
+
+</td>
+
+<td>
+<img src="assets/geographical.png" width="25%"> <br>
+<font size="6" >Location</font> <br>
+<font size="5" >kolkata</font>
+
+</td>
+</tr>
+
+<tr bgcolor="black" align="center"> 
+<td colspan="3"> <font color="#fff"> copyright @ 2024 | Souvik Pratihar </font></td>
+</tr>
+</table>
+
+</td></tr>
+</table>
+
+<!--- End Footer--->
+
+</body>
+</html>
